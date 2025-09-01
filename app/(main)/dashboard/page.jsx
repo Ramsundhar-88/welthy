cc0b2597241b3c042ced9ef7a55fe4523837cc9d
Ramsundhar-88 <Ramsundhar.sades@gmail.com>
2025-09-01 14:34:44 +0530

    App drawer implemted for dashboard

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,8 +1,61 @@
+import CreateAccountDrawer from '@/components/create-account-drawer'
+import { CardContent,Card} from '@/components/ui/card'
+import { Plus } from 'lucide-react'
 import React from 'react'
 
 const DashBoard = () => {
   return (
-    <div className='px-5'>Page</div>
+    <div className='px-5'>
+
+    <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
+      <CreateAccountDrawer>
+        <Card className="hover:shadow-md transition-shadwo cursor-pointer">
+          <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
+            <Plus className="h-10 w-10 mb-2"/>
+            <p className='text-sm font-medium'>Add new Account</p>
+
+          </CardContent>
+        </Card>
+
+      </CreateAccountDrawer>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    
   )
 }
 
