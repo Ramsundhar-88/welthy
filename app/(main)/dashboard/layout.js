6cf2c242ebd92a65caf849dbb8f19e0de7cd205b
Ramsundhar-88 <Ramsundhar.sades@gmail.com>
2025-09-01 11:15:07 +0530

    Bashboard logic implemted for creation of account

---
+++ app/(main)/dashboard/layout.js
@@ -0,0 +1,18 @@
+import React, { Suspense } from 'react'
+import DashBoard from './page'
+import {BarLoader} from "react-spinners"
+
+const Dashboardlayout = () => {
+  return (
+    <div className='px-5'>
+         <h1 className='text-6xl font-bold mb-5'>Dashboard</h1>
+         <Suspense fallback={<BarLoader className='mt-4' width={"100%"} color="#9333ea"/>}>
+            <DashBoard/>
+         </Suspense>
+         
+      
+    </div>
+  )
+}
+
+export default Dashboardlayout
