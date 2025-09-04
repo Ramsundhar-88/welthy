6943e8ac86c02396c937f43e5d8aa618ad3af4d7
Ramsundhar-88 <Ramsundhar.sades@gmail.com>
2025-09-04 23:10:27 +0530

    account backend done

---
+++ actions/account.js
@@ -0,0 +1,60 @@
+import { db } from "@/lib/prisma"
+import { auth } from "@clerk/nextjs/server"
+import { revalidatePath } from "next/cache"
+import { success } from "zod"
+
+const serializeTranscation=(obj)=>{
+    const serailized = {...obj}
+
+    if(obj.balance){
+        serailized.balance=obj.balance.toNumber()
+    }
+    return serailized
+
+    if(obj.amount){
+        serailized.amount=obj.amount.toNumber()
+    }
+    return serailized
+
+}
+
+export async function updateDefaultAccount(accountId){
+    try{
+        const {userId} = await auth()
+        if(!userId) throw new Error("Unauthorized")
+
+        
+        const user = await db.user.findUnique({
+            where: { clerkUserId: userId },
+            });
+
+        if(!user){
+            throw new Error("User not found")
+        }
+
+        await db.account.updateMany({
+            where:{userId:user.id,isDefault:true},
+            data:{isDefault:false}
+        })
+
+        const account = db.account.update({
+            where:{
+                id:accountId,
+                userId:user.id
+            },
+            data:{
+                isDefault:true
+
+            }
+        })
+
+
+        revalidatePath('/dashboard')
+
+        return {success:true,data:serializeTranscation(account)}
+        
+    }catch(error){
+        return {success:false,error:error.message}
+
+    }
+}
