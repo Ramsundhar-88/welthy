01d55efd00029f5fa1e9012a9b6fbe72ee02145f
Ramsundhar-88 <Ramsundhar.sades@gmail.com>
2025-10-15 11:42:50 +0530

    Mobile responsive

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import { Toaster } from "sonner";
 const inter = Inter ({  subsets: ["latin"],})
 
 export const metadata = {
-  title: "Finance App",
+  title: "Welthy",
   description: "Calculate your finances easily",
 };
 
